c1203a50de371868c1376e5cdba1dc42c4124e41
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-12 23:23:33 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,15 @@ class String
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentence = self.split(/[?!\.]{1,10}/)
+    return sentence.count
   end
 end
